3a1b0c1e4ffe335e41ddcdf5a3624d7770423ecc
Dan <pinerodanchristian@gmail.com>
2022-11-09 21:16:51 -0800

    note

--- DSA-A2-Array-Mod.py
+++ DSA-A2-Array-Mod.py
@@ -5,3 +5,10 @@
 # - allow user to select item in the menu (check if valid)
 # - perform the selected option (you may prompt additional info to user when need)
 # - display the resulting values of the array
+
+# Note: 
+# The program has an array variable containing 10 random numbers
+# You may add other options and features
+# Your program should be uploaded to github before 1:30pm
+# Record a demo presenting your program
+# Send the demo directly to my messenger
