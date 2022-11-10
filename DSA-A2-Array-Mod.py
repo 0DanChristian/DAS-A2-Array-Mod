dc233daba84d1227c8074a3f22b718ba908345a4
Dan <pinerodanchristian@gmail.com>
2022-11-09 21:43:27 -0800

    final

--- DSA-A2-Array-Mod.py
+++ DSA-A2-Array-Mod.py
@@ -91,21 +91,21 @@ def open_del_e():
         else:
             print("Error! Your number was not in the list!")
 
-# 'open_sum_e', determines the sum of all the elements in the list
+# 'open-sum-e', determines the sum of all the elements in the list
 def open_sum_e():
     listsum = sum(genlist)
     print("\nThe sum of the values in the list is: ", listsum, "\n")
     print("\nYour New Array: ", genlist, "\n")
     main()
 
-# 'open_asc_l', sorts the list in ascending order
+# 'open-asc-l', sorts the list in ascending order
 def open_asc_l():
     genlist.sort()
     print("The list has been arranged in ascending order!")
     print("\nYour New Array: ", genlist, "\n")
     main()
 
-# 'open_dsc_l', sorts the list in descending order
+# 'open-dsc-l', sorts the list in descending order
 def open_dsc_l():
     genlist.sort()
     genlist.reverse()
@@ -113,3 +113,82 @@ def open_dsc_l():
     print("\nYour New Array: ", genlist, "\n")
     main()
 
+# 'main', calls the other functions
+def main():
+    list_menu()
+    while True:
+        choice = input("\nEnter your choice[1-7]: ")
+        if choice == '1':
+            string = '\n'"Reverse the List " + "selected!"
+            dotted = '\n'+ len(string) * "-"
+            
+            print(dotted,
+                  string,
+                  dotted)
+            
+            open_rev_l()
+            break
+
+        elif choice == '2':
+            string = '\n'"Add an element " + "selected!"
+            dotted = '\n'+ len(string) * "-"
+            
+            print(dotted,
+                  string,
+                  dotted)
+            
+            open_add_e()
+            break
+
+        elif choice == '3':
+            string = '\n'"Delete an element " + "selected!"
+            dotted = '\n'+ len(string) * "-"
+            
+            print(dotted,
+                  string,
+                  dotted)
+            
+            open_del_e()
+            break
+
+        elif choice == '4':
+            string = '\n'"Find the sum " + "selected!"
+            dotted = '\n'+ len(string) * "-"
+            
+            print(dotted,
+                  string,
+                  dotted)
+            
+            open_sum_e()
+            break
+
+        elif choice == '5':
+            string = '\n'"Arrange in ascending order " + "selected!"
+            dotted = '\n'+ len(string) * "-"
+            
+            print(dotted,
+                  string,
+                  dotted)
+            
+            open_asc_l()
+            break
+
+        elif choice == '6':
+            string = '\n'"Arrange in descending order " + "selected!"
+            dotted = '\n'+ len(string) * "-"
+            
+            print(dotted,
+                  string,
+                  dotted)
+            
+            open_dsc_l()
+            break
+
+        elif choice == '7':
+            print ("Thanks for playing!\n")
+            break
+                         
+        print("Error! Invalid input. Press any key to continue...\n")
+        
+main()        
+        
